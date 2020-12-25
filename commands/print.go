e2628250e78634a14d41af54da7eb23a82ed93d8
Yelena <lena.skripnik.01@gmail.com>
2020-12-25 14:14:51 +0200

    Update print.go

--- commands/print.go
+++ commands/print.go
@@ -1,1 +1,15 @@
+package commands
 
+import (
+	"fmt"
+
+	"github.com/stormtrooper01/cse_lab4/engine"
+)
+
+type printCommand struct {
+	Arg string
+}
+
+func (p *printCommand) Execute(loop engine.Handler) {
+	fmt.Println(p.Arg)
+}
