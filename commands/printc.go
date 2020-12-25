79b6268212f34c0b33f6c13370fd39a88969a210
Yelena <lena.skripnik.01@gmail.com>
2020-12-25 14:16:07 +0200

    Update printc.go

--- commands/printc.go
+++ commands/printc.go
@@ -1,1 +1,16 @@
+package commands
 
+import (
+	"strings"
+	"github.com/stormtrooper01/cse_lab4/engine"
+)
+
+type printcCommand struct {
+	Count  int
+	Symbol string
+}
+
+func (p *printcCommand) Execute(loop engine.Handler) {
+	repeated := strings.Repeat(p.Symbol, p.Count)
+	loop.Post(&printCommand{Arg: repeated})
+}
