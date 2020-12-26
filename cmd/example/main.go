c4a9e58e273285d83c20bc0a870bedbd98ed9297
SonyaSytnik <sofia.sytnik2012@gmail.com>
2020-12-26 21:17:16 +0200

    Update file

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+  "bufio"
+  "flag"
+  "os"
+  "strings"
+
+  "github.com/stormtrooper01/cse_lab4/commands"
+  "github.com/stormtrooper01/cse_lab4/engine"
+)
+
+func parse(commandline string) engine.Command {
+  commandFields := strings.Fields(commandline)
+  commandName, args := commandFields[0], commandFields[1:]
+  command := commands.Construct(commandName, args)
+  return command
+}
+
+func main() {
+  inputFile := flag.String("f", "", "File to run with EventLoop")
+  flag.Parse()
+
+  if input, err := os.Open(*inputFile); err == nil {
+    eventLoop := new(engine.EventLoop)
+    eventLoop.Start()
+    defer input.Close()
+    scanner := bufio.NewScanner(input)
+    for scanner.Scan() {
+      eventLoop.Post(parse(scanner.Text()))
+    }
+    eventLoop.AwaitFinish()
+  } else {
+    flag.PrintDefaults()
+    os.Exit(1)
+  }
+}
